b2f173fba9a8cf15ce5512c3aa157430e4217a4f
anil1973-13-05 <101050670+anil1973-13-05@users.noreply.github.com>
2022-03-13 20:54:08 +0530

    Add files via upload

---
+++ Mar12/script.js
@@ -0,0 +1,60 @@
+let weather = {
+    apiKey: "98c09e32bd8a1a3dbd7db6123f34c1e1",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+      var city = document.querySelector(".city");
+      city.innerText = "Weather in " + name;
+      var icon1 = document.querySelector(".icon");
+      icon1.src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+        var desc = document.querySelector(".description");
+      desc.innerText = description;
+      var temperature = document.querySelector(".temp");
+      temperature.innerText = temp + "°C";
+      var humd = document.querySelector(".humidity")
+     humd.innerText = "Humidity: " + humidity + "%";
+     var wind1 = document.querySelector(".wind");
+      wind1.innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Visakhapatnam");
